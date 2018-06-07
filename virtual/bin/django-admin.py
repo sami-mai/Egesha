b05435b1392c5aa8f6ca5301ab08fc9d981b416b
sami-mai <samirah.maison@gmail.com>
2018-06-07 13:12:00 +0300

    modify

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/sami_mai/Projects/ms-projects/Core/python3.6/django/proDev/egesha/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
