f86b02aa6d4e942507c7299a7036e5246a1c020a
Mary Mbugua <marymbugua.nm@gmail.com>
2018-06-07 05:16:51 +0300

    view function to render index/dashboard

--- lotManager/views.py
+++ lotManager/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Guard
+from django.contrib.auth.models import User
 
 # Create your views here.
+def landing(request):
+    
+    
+
+    return render(request,'index.html')
