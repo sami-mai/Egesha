4281b974e23c908a0098657fce6e8f43c28ae43a
Mary Mbugua <marymbugua.nm@gmail.com>
2018-06-07 05:12:47 +0300

    class guard on models.py

--- lotManager/models.py
+++ lotManager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+#import security profile
+#import lot details
 
 # Create your models here.
+
+class Guard(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    lot = models.ForeignKey(Lot,on_delete=models.CASCADE)
+
+    #method to edit stuff..lot details
