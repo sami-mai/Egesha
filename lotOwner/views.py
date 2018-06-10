b523bc5ba763182a4e3cc14b5de2c46acc3279ed
MaryAnne45 <mary.jereh@gmail.com>
2018-06-10 17:27:43 +0300

    add changes to lot owner models and forms

--- lotOwner/views.py
+++ lotOwner/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from accounts.models import OwnerProfile
 # Create your views here.
 
 def home(request):
     title='Welcome lot owner'
     return render (request,'Lot/home.html',{"title":title})
+def Lotdetail(request,profile_id):
+    current_profile=
