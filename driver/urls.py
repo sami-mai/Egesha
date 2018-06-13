269b542826ae7ed0244aa57bf16f7e38ed110dbf
Mary Mbugua <marymbugua.nm@gmail.com>
2018-06-13 19:19:15 +0300

    payment done- mpesa integration

--- driver/urls.py
+++ driver/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^edit-profile/',views.edit_profile, name = 'edit_profile'),
     url(r'^car-details/',views.car_details, name = 'car_details'),
     url(r'^$',views.home, name ='home'),
+    url(r'^pay/$', views.trigger_payment, name='trigger_payment')
 
 ]
 if settings.DEBUG:
