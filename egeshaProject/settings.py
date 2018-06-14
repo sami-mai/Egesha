2497379ba81f59327b923d4fc202748147d8c617
kasambuli <cynthiakasambuli@gmail.com>
2018-06-14 19:32:16 +0300

    merge

--- egeshaProject/settings.py
+++ egeshaProject/settings.py
@@ -92,17 +92,10 @@ WSGI_APPLICATION = 'egeshaProject.wsgi.application'
 
 DATABASES = {
    'default': {
-<<<<<<< HEAD
          'ENGINE': config('DB_ENGINE'),
          'NAME': config('DB_NAME'),
          'USER': config('DB_USER'),
          'PASSWORD': config('DB_PASSWORD'),
-=======
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'owner',
-         'USER': 'maryanne',
-         'PASSWORD': '1234@5',
->>>>>>> 91a30474235bb6101f0fa6cdd2fdf67d224a73cb
          }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
