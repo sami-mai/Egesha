30b5934508a38a5287b05838608385608e57bd2f
kasambuli <cynthiakasambuli@gmail.com>
2018-06-14 14:52:02 +0300

    add links to landing page

--- driver/urls.py
+++ driver/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'driver'
 
 urlpatterns = [
     url(r'^search/',views.search_location,name='Search'),
