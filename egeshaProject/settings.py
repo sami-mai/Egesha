cdc79bf18e899232f172e100df72c2890c0f52fa
sami-mai <samirah.maison@gmail.com>
2018-06-17 20:19:51 +0300

    style footer

--- egeshaProject/settings.py
+++ egeshaProject/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
    'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'egesha',
-            'USER': 'nish',
-            'PASSWORD': 'Nish',
+            'USER': 'sami_mai',
+            'PASSWORD': 'SmaiDB',
          }
 }
 
