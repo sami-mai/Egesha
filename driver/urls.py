6bcf64eb27e9e387da72f1c8aa69ac503f443f05
Mary Mbugua <marymbugua.nm@gmail.com>
2018-06-17 18:26:07 +0300

    payment redirect

--- driver/urls.py
+++ driver/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     url(r'^edit-profile/',views.edit_profile, name = 'edit_profile'),
     url(r'^car-details/',views.car_details, name = 'car_details'),
     url(r'^$',views.home, name ='home'),
-    url(r'^pay/$', views.trigger_payment, name='trigger_payment')
+    url(r'^pay/$', views.trigger_payment, name='trigger_payment'),
+    url(r'^status/$', views.status, name='status')
 
 ]
 if settings.DEBUG:
